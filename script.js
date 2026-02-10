a574ac2efe5e8c4b47eb20b94ec9157361799572
jasoneroy <jasoneroy45@gmail.com>
2026-02-10 13:56:08 +0530

    Downlader

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-var main = document.querySelector('main');
-var btn = document.querySelector('button');
+var btn = document.querySelector('button')
+var h1 = document.querySelector('h1')
+var box = document.querySelector('#inner')
+var p = document.querySelector('p')
 
 btn.addEventListener('click',function(){
-    var div = document.createElement('div')
-    var h3 = document.createElement('h3');
-    div.style.height = '50px'
-    div.style.width = '50px'
-    var x = Math.floor(Math.random()*100)
-    var y = Math.floor(Math.random()*100)
-    var c1 = Math.floor(Math.random()*256)
-    var c2 = Math.floor(Math.random()*256)
-    var c3 = Math.floor(Math.random()*256)
-    div.style.backgroundColor = `rgb(${c1},${c2},${c3})`
-    div.style.position = 'absolute'
-    main.appendChild(div)
-    div.style.top = x + '%'
-    div.style.left = y + '%'
-    div.style.borderRadius = '10px'
-    div.appendChild(h3)
-    h3.innerHTML = 'HI'
-    div.style.display = 'flex'
-    div.style.justifyContent = 'center'
-    div.style.alignItems = 'center'
-    h3.style.color = `rgb(${c2},${c3},${c1})`
+    let time = Math.floor(Math.random()*101) + 10
+    console.log(time)
+    var a = 0
+    btn.style.pointerEvents = 'none'
+    let loader = setInterval(function(){
+        a++
+        box.style.width = a + '%'
+        h1.innerHTML = a+'%'
+    },time)
 
+    setTimeout(() => {
+        clearInterval(loader)
+        p.innerHTML = `Your download is completed in ${time/10} seconds.`
+        btn.innerHTML = "Downloaded"
+        btn.style.opacity = '0.5'
+    },time*100);
+
+ 
 })
